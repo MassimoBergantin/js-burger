ca102ab45bb71b7ffe7a81823c3ebed7e86d261d
MassimoBergantin <maxbergax@gmail.com>
2021-06-08 18:44:26 +0200

    burger

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-alert("Per uon sconto del 20% inserisci il codice ");
+
 
 var createSum = document.getElementById('somma-button');
 
 var resultSumElement = document.getElementById('result-sum');
+
+var userCoupon = document.getElementById("discount");
+
+var price = document.getElementById('price');
 
 createSum.addEventListener('click', function() {
 
@@ -14,10 +18,14 @@ createSum.addEventListener('click', function() {
 
         if (checkNum[i].checked) {
 
-             resultSumInt += parseInt (checkNum[i].value)
+             resultSumInt += parseInt (checkNum[i].value);
 
         }
-    }
+    } if (userCoupon.value.length === 6){
+        document.getElementById('price').innerHTML =  " € "  + parseInt(resultSumInt - 20 / 100).toFixed(2);
+    } else{
+        document.getElementById('price').innerHTML =  " € "  +  resultSumInt.toFixed(2);  
 
-    resultSumElement.innerHTML = '€' + resultSumInt;
+
+}
 })
